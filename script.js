872adb460a8841191188715acf9240895b334c8e
Alex <hunt.alex.e@gmail.com>
2021-09-07 14:41:45 -0700

    added ui display functionality for pressed keys

--- script.js
+++ script.js
@@ -1,43 +1,78 @@
-function add(nums) {
-    sum = 0
-    for (i = 0; i < nums.length; i++) {
-        sum += nums[i];
+// add event listener to calculator buttons
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator-keys');
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = screenContent.textContent
+
+        if (!action) {
+            if (displayedNum === '0') {
+                screenContent.textContent = keyContent
+            } else {
+                screenContent.textContent = displayedNum + keyContent
+            }
+        }
+
+
+        if (action === 'decimal') {
+            screenContent.textContent = displayedNum + keyContent
+        }
+
+        if (action === 'add') {
+            screenContent.textContent = displayedNum + ' ' + keyContent + ' '
+        }
+
+        if (action === 'subtract') {
+            screenContent.textContent = displayedNum + ' ' + keyContent + ' '
+        }
+
+        if (action === 'multiply') {
+            screenContent.textContent = displayedNum + ' ' + keyContent + ' '
+        }
+
+        if (action === 'divide') {
+            screenContent.textContent = displayedNum + ' ' + keyContent + ' '
+        }
+
+        if (action === 'equal') {
+            const secondNum = displayedNum
+
+        }
+
+
+        if (!action) {
+            console.log('number key!')
+          }
+        
+          if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+          ) {
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
+            console.log('operator key!')
+          }
+        
+        if (action === 'decimal') {
+            console.log('decimal key!')
+        }
+        
+        if (action === 'clear') {
+            console.log('clear key!')
+        }
+        
+        if (action === 'delete') {
+            console.log('delete key!')
+        }
+        
+        if (action === 'equal') {
+            console.log('equal key!')
+        }
     }
-    return sum;
-}
-
-function subtract(nums) {
-    sum = nums[0]
-    for (i = 0; i < nums.length; i++) {
-        sum -= nums[i];
-    }
-
-    return sum;
-}
-
-function multiply(nums) {
-    sum = nums[i]
-    for (i = 0; i < nums.length; i++) {
-        sum *= nums[i];
-    }
-    return sum;
-}
-
-function divide(nums) {
-    sum = nums[i]
-    for (i = 0; i < nums.length; i++) {
-        sum /= nums[i];
-    }
-    return sum;
-}
-
-// function screenValue() {
-//     let button = document.getElementsByClassName('.btn')
-
-//     for (let i = 0; i < button.length; i++) {
-//         button[i].addEventListener('click', function () {
-//             var x = button.value;
-            
-//         }
-//     }
-// }
+   })
